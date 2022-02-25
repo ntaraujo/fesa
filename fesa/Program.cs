4d0673d42fdd593c8a0c78ebe3afa25f9842ac82
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-02-24 22:42:48 -0300

    Update Program.cs

--- fesa/Program.cs
+++ fesa/Program.cs
@@ -10,6 +10,25 @@ namespace fesa
     {
         static void Main(string[] args)
         {
+            // criando variaveis
+
+            String nome;
+            Int16 idade;
+
+            // entrada de dados
+
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Digite sua idade: ");
+            idade = Int16.Parse(Console.ReadLine());
+
+            // saida de dados
+            Console.WriteLine();
+            Console.Write($"Olá {nome}, seja bem vindo. \n");
+            Console.Write($"Você tem {idade} anos de idade");
+
+            Console.ReadKey();
         }
     }
 }
