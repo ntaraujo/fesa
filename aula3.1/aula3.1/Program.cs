56abb552d67f74660e8f81f4b1e89984c36ffa43
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-03-07 22:24:32 -0300

    ex10

---
+++ aula3.1/aula3.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Int16 ano;
+
+            Console.Write("Qual o ano a verificar? ");
+            ano = Int16.Parse(Console.ReadLine());
+
+            if (ano % 4 == 0)
+            {
+                Console.Write("Esse ano é bissexto");
+            }
+            else
+            {
+                Console.Write("Esse ano não é é bissexto");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
