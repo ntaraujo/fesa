91bcf0889ae708a9c61f8107c6f8eb05ceba9a41
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-03-07 22:34:03 -0300

    ex11

---
+++ aula3.2/aula3.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Int16 ano;
+
+            Console.Write("Qual o número a verificar? ");
+            ano = Int16.Parse(Console.ReadLine());
+
+            if (ano % 2 == 0)
+            {
+                Console.Write("Esse número é par");
+            }
+            else
+            {
+                Console.Write("Esse número não é par");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
