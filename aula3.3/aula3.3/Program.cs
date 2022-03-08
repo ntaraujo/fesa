c6651d1a1b2ba034503a3f1915076cd59196aef3
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-03-07 22:45:34 -0300

    ex9

---
+++ aula3.3/aula3.3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Int16 dia;
+
+            Console.Write("Qual o número do dia da semana? ");
+            dia = Int16.Parse(Console.ReadLine());
+
+            if (dia == 1)
+            {
+                Console.Write("Esse dia é Domingo");
+            }
+            else if (dia == 2)
+            {
+                Console.Write("Esse dia é Segunda");
+            }
+            else if (dia == 3)
+            {
+                Console.Write("Esse dia é Terça");
+            }
+            else if (dia == 4)
+            {
+                Console.Write("Esse dia é Quarta");
+            }
+            else if (dia == 5)
+            {
+                Console.Write("Esse dia é Quinta");
+            }
+            else if (dia == 6)
+            {
+                Console.Write("Esse dia é Sexta");
+            }
+            else
+            {
+                Console.Write("Esse dia é Sábado");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
