9b4fe49748a6fb87f2c610c12c73216a6f4d11ca
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-03-31 22:18:15 -0300

    6.1 loop

---
+++ aula6.1/aula6.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i > -5; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
