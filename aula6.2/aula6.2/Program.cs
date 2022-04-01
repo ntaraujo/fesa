8754f8c101d7de09fcc96cd193285549a1b743c7
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-03-31 22:25:03 -0300

    pré-incremento e  pós-incremento

---
+++ aula6.2/aula6.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b;
+
+            b = a++;
+            Console.WriteLine($"A = {a}\nB = {b}");
+
+            Console.ReadKey();
+        }
+    }
+}
