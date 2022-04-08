150caa9b4e45e90e5a3ace45a05e5c5adbff87a5
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-04-07 22:43:56 -0300

    aula8.2 palíndromo

---
+++ aula8.2/aula8.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula8._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+            bool palindromo = true;
+
+            Console.Write("Digite uma palavra ou frase: ");
+            texto = Console.ReadLine().Replace(" ", "");
+
+            for (int i = 0; i<texto.Length/2; i++)
+            {
+                if (texto[i] != texto[texto.Length - i - 1])
+                {
+                    palindromo = false;
+                    break;
+                }
+            }
+
+            if (palindromo)
+            {
+                Console.WriteLine("É um palíndromo");
+            }
+            else
+            {
+                Console.WriteLine("Não é um palíndromo");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
