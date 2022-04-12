a5635e75fbd4702e5ce9e8beccc2dcf791c6d9c2
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-04-11 21:36:19 -0300

    aula9 beecrowd 1006 media

---
+++ aula9/aula9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c;
+
+            a = float.Parse(Console.ReadLine());
+            b = float.Parse(Console.ReadLine());
+            c = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"MEDIA = {(a * 2 + b * 3 + c * 5) / 10}");
+
+            Console.ReadKey();
+        }
+    }
+}
