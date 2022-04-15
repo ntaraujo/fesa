3c5c1d714bf64c3aa132cd8d48355a6356383754
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-04-14 22:20:37 -0300

    aula10

---
+++ aula10/aula10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] nomes = new string[5];
+
+            for (Int16 ind=0; ind<nomes.Length; ind++)
+            {
+                Console.Write($"Digite o nome[{ind}]: ");
+                nomes[ind] = Console.ReadLine();
+            }
+        }
+    }
+}
