f4324213462658c8f8c6652b28014feba2543dda
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-04-18 22:42:56 -0300

    aula11 ascii

---
+++ aula11/aula11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] ascii = Encoding.ASCII.GetBytes("A");
+            Console.WriteLine(ascii[0]);
+
+            Console.WriteLine(char.ConvertFromUtf32(65));
+
+            char x = 'A';
+            Console.WriteLine((byte)x);
+
+            int y = 66;
+            Console.WriteLine((char)y);
+
+            for (int carac = 32; carac <= 126; carac++)
+            {
+                Console.Write($"{(char)carac}-");
+            }
+        }
+    }
+}
