1ce885049c08b36aa749b0322e91b6db064a6f10
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-05-05 22:23:50 -0300

    aula13.1 pares em linhas de matriz

---
+++ aula13.1/aula13.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula13._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int[,] matriz = new int[4, 3];
+            int[] pares = { 0, 0, 0, 0 };
+            int numero;
+
+            // preenchendo
+            for (int linha = 0; linha < 4; linha++)
+            {
+                for (int coluna = 0; coluna < 3; coluna++)
+                {
+                    Console.Write($"Digite a matriz[{linha}, {coluna}]: ");
+                    numero = int.Parse(Console.ReadLine());
+
+                    matriz[linha, coluna] = numero;
+                    if (numero % 2 == 0) pares[linha]++;
+                }
+                Console.WriteLine();
+            }
+
+            for (int linha = 0; linha < matriz.GetLength(0); linha++)
+            {
+                Console.WriteLine($"Pares na linha {linha}: {pares[linha]}");
+            }
+
+        }
+    }
+}
