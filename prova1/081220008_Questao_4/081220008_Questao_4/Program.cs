b1b556fbc49afc152931c2f4f6a998677895e59d
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-05-26 22:45:34 -0300

    questao4 n2 2bi 2022

---
+++ prova1/081220008_Questao_4/081220008_Questao_4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _081220008_Questao_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[1000, 1000];
+            Random rnd = new Random();
+            bool identidade = true;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    matriz[i, j] = rnd.Next(0, 2);
+
+                    if ((i == j && matriz[i, j] == 0) || (i != j && matriz[i, j] == 1))
+                    {
+                        identidade = false;
+                    }
+                }
+            }
+
+            if (identidade)
+            {
+                Console.WriteLine("A matriz é uma matriz identidade");
+            }
+            else
+            {
+                Console.WriteLine("A metriz não é uma matriz identidade");
+            }
+        }
+    }
+}
