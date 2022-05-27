fc3a0e81917520d6f4f69a5f9e8f66ba759e2d24
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-05-26 22:20:10 -0300

    questao3 n2 2bi 2022

---
+++ prova1/081220008_Questao_3/081220008_Questao_3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _081220008_Questao_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool errado;
+            string[] nomes = new string[100];
+            string nome_aux;
+            int[] idades = new int[100];
+            int idade_aux;
+            int maior_idade = 0, index_maior_idade = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                do
+                {
+                    Console.Write("Seu nome completo: ");
+                    nome_aux = Console.ReadLine();
+
+                    if (nome_aux.IndexOf(' ') == -1)
+                    {
+                        Console.WriteLine("O nome tem que ter pelo menos 2 palavras.");
+                        errado = true;
+                    }
+                    else
+                    {
+                        nomes[i] = nome_aux;
+                        errado = false;
+                    }
+                } while (errado);
+
+                do
+                {
+                    Console.Write("Sua idade: ");
+                    idade_aux = int.Parse(Console.ReadLine());
+
+                    if (idade_aux > 80 || idade_aux < 0)
+                    {
+                        Console.WriteLine("A idade precisa estar entre 0 e 80");
+                        errado = true;
+                    }
+                    else
+                    {
+
+                        idades[i] = idade_aux;
+                        errado = false;
+
+                        if (idade_aux > maior_idade)
+                        {
+                            maior_idade = idade_aux;
+                            index_maior_idade = i;
+                        }
+                    }
+                } while (errado);
+            }
+
+            Console.WriteLine($"A pessoa mais velha tem {maior_idade} anos. Seu nome é \"{nomes[index_maior_idade]}\"");
+        }
+    }
+}
