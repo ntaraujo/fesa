fca4e5c51a2f4da324d2c18c3de328ab3ae17395
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-08-09 20:23:37 -0300

    cria aula15.4

---
+++ 2sem/aula15.4/aula15.4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace aula15._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = new int[3];
+
+            Console.Write("Valor 1: ");
+            valores[0] = int.Parse(Console.ReadLine());
+
+            Console.Write("Valor 2: ");
+            valores[1] = int.Parse(Console.ReadLine());
+
+            Console.Write("Valor 3: ");
+            valores[2] = int.Parse(Console.ReadLine());
+
+            Array.Sort(valores);
+
+            foreach (int valor in valores)
+            {
+                Console.Write(valor + " ");
+            }
+        }
+    }
+}
