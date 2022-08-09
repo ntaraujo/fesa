585a09dd8230f1180d254b1a06043fc53d21e5fe
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-08-09 19:44:05 -0300

    cria aula 15.2

---
+++ 2sem/aula15.2/aula15.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace aula15._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Valor 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Valor 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int num1antigo = num1;
+
+            num1 = (num2 > num1) ? num2 : num1;
+            num2 = (num1 != num1antigo) ? num1antigo : num2;
+
+            Console.WriteLine(num1 + " " + num2);
+
+        }
+    }
+}
