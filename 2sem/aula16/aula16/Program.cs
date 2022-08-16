b3b063d0f963d24bccec85f359a9b6926bbf8abc
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-08-16 19:54:29 -0300

    cria aula16

---
+++ 2sem/aula16/aula16/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace aula16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Digite um número: ");
+                int num = int.Parse(Console.ReadLine());
+
+                string saida = "";
+
+                do
+                {
+                    saida = $"{num % 2}{saida}";
+                    num /= 2;
+                } while (num >= 2);
+                if (num % 2 != 0)
+                {
+                    saida = $"1{saida}";
+                }
+
+                Console.WriteLine($"Em binário: {saida}");
+            }
+        }
+    }
+}
