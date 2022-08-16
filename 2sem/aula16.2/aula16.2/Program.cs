1196f2cdfac83f89a709fda8de79f631f4e61eec
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-08-16 20:25:36 -0300

    cria aula16.2

---
+++ 2sem/aula16.2/aula16.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace aula16._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            for (int i = 0; i < 100000000; i++)
+            {
+                while (true)
+                {
+                    Console.Write("Digite uma base menor ou igual a 10: ");
+                    int baseNum = int.Parse(Console.ReadLine());
+                    if (baseNum > 10) continue;
+
+                    Console.Write("Digite um número: ");
+                    int num = int.Parse(Console.ReadLine());
+
+                    string saida = "";
+
+                    do
+                    {
+                        saida = $"{num % baseNum}{saida}";
+                        num /= baseNum;
+                    } while (num >= 2);
+                    if (num % baseNum != 0)
+                    {
+                        saida = $"1{saida}";
+                    }
+
+                    Console.WriteLine($"Na base {baseNum}: {saida}");
+                }
+            }
+        }
+
+    }
+}
