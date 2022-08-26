40c6c9cf4865b71f05d1b71f411090bb5cee493e
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-08-25 21:52:10 -0300

    simplifica aula17

--- 2sem/aula17/aula17/Program.cs
+++ 2sem/aula17/aula17/Program.cs
@@ -13,12 +13,12 @@ namespace aula17
 
                 int rg_soma = 0;
 
-                for (int i = 0, ajudador = 9; i < 8; i++, ajudador--)
+                for (int i = 0; i < 8; i++)
                 {
-                    rg_soma += (rg[i] - '0') * ajudador;
+                    rg_soma += (rg[i] - '0') * (9 - i);
                 }
 
-                bool valido = rg_soma % 11 == rg[rg.Length - 1] - '0';
+                bool valido = rg_soma % 11 == rg[8] - '0';
                 Console.WriteLine("O RG é " + (valido ? "válido" : "falso"));
             }
         }
