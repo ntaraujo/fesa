08549bce167e3659a18710271f95e534fb6fb56d
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-08-30 20:46:57 -0300

    ex10 menor valor

---
+++ 2sem/aula18.5/aula18.5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace aula18._5
+{
+    class Program
+    {
+        static int MenorValor(int[] valores)
+        {
+            int menor = valores[0];
+            
+            foreach (int valor in valores)
+            {
+                if (valor < menor)
+                {
+                    menor = valor;
+                }
+            }
+
+            return menor;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] valores = { 1, 4, 0 };
+            Console.WriteLine(MenorValor(valores));
+        }
+    }
+}
