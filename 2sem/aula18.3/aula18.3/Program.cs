f5d0c11d315da48ad8d3fc114f8a0b1fb6757af5
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-08-30 20:27:44 -0300

    ex5 nome invertido

---
+++ 2sem/aula18.3/aula18.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace aula18._3
+{
+    class Program
+    {
+        static string NomeInvertido(string nome)
+        {
+            char[] invertido = new char[nome.Length];
+
+            for (int i = 0; i < nome.Length; i++)
+            {
+                invertido[i] = nome[nome.Length - 1 - i];
+            }
+
+            return new string(invertido);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um nome: ");
+            Console.WriteLine(NomeInvertido(Console.ReadLine()));
+        }
+    }
+}
