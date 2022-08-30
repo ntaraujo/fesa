f843d30f291fe00aa0e640a64f0b2510d3ebb068
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-08-30 19:51:26 -0300

    ex7 verifica espaço

---
+++ 2sem/aula18/aula18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace aula18
+{
+    class Program
+    {
+        static bool TemEspaco(string texto)
+        {
+            return texto.Contains(' ');
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Digite algo: ");
+            if (TemEspaco(Console.ReadLine()))
+            {
+                Console.WriteLine("O que você digitou tem espaço");
+            }
+            else
+            {
+                Console.WriteLine("O que você digitou não tem espaço");
+            }
+        }
+    }
+}
