e235718fc81fa7b0d18dedb9fb8f178dfe919922
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-08-30 20:52:29 -0300

    comentários batalha naval

--- 2sem/batalha_naval/batalha_naval/Program.cs
+++ 2sem/batalha_naval/batalha_naval/Program.cs
@@ -6,10 +6,12 @@ namespace batalha_naval
     {
         static void Main(string[] args)
         {
+            // PPPPP CCCC CCCC DDD DDD DDD SS SS SS
             char[] simbolos =    {'P', 'C', 'D', 'S'};
             int[]  tamanhos =    { 5,   4,   3,   2 };
             int[]  quantidades = { 1,   2,   3,   3 };
 
+            // enche o tabuleiro de água
             char[] tabuleiro = new char[60];
             for (int i = 0; i < 60; i++)
             {
@@ -23,11 +25,14 @@ namespace batalha_naval
             for (int i = 0, contador = 0; i < 4;)
             {
                 tamanho_esperado = tamanhos[i];
+
+                // posição máxima pra posicionar é o tamanho do tabuleiro menos o da embarcação
                 posicao_aleatoria = aleatorio.Next(59-tamanho_esperado);
 
                 tamanho_esperado_disponivel = true;
                 for (int i_posicao = 0; i_posicao < tamanho_esperado; i_posicao++)
                 {
+                    // é tudo água onde eu quero colocar a embarcação?
                     if (tabuleiro[posicao_aleatoria + i_posicao] != 'A')
                     {
                         tamanho_esperado_disponivel = false;
