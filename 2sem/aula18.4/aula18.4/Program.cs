7a3e186ab2fc694b493cb2a00d4d1cb543792db2
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-08-30 20:37:10 -0300

    ex9 tabuada

---
+++ 2sem/aula18.4/aula18.4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace aula18._4
+{
+    class Program
+    {
+        static void MostraTabuada(int numero)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero * i}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            MostraTabuada(int.Parse(Console.ReadLine()));
+        }
+    }
+}
