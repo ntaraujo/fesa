2726ed6207c5a446ad7ba9559dfdd7395336f334
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-08-30 20:08:49 -0300

    ex4 vogais

---
+++ 2sem/aula18.2/aula18.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace aula18._2
+{
+    class Program
+    {
+        static void EscreveVogais(string nome)
+        {
+            foreach (char letra in nome.Normalize())
+            {
+                if ("aeiou".Contains(letra))
+                {
+                    Console.Write(letra);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um nome: ");
+            EscreveVogais(Console.ReadLine());
+        }
+    }
+}
