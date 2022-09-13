12683af0de2a79ba935279a3e0029668ef86807d
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-09-13 20:50:09 -0300

    cria aula19

---
+++ 2sem/alg/aula19/aula19/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace aula19
+{
+    class Program
+    {
+        static string ARQUIVO = @"Z:\repos\fesa\2sem\alg\aula19\teste.txt";
+        static void Main(string[] args)
+        {
+            bool continuar = true;
+            while (continuar)
+            {
+                Console.Write("Nova estudante: ");
+                string novaLinha = Console.ReadLine();
+
+                if (novaLinha.Length == 0)
+                {
+                    continuar = false;
+                }
+                else
+                {
+                    File.AppendAllText(ARQUIVO, novaLinha);
+                }
+                
+            }
+        }
+    }
+}
