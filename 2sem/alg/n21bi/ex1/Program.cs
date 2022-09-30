9a398849df48bc4ac09556ce9544509bbf590ee4
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-09-29 21:51:14 -0300

    create n21bi\ex1

---
+++ 2sem/alg/n21bi/ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ex1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Número natural: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    Console.WriteLine("É composto");
+                    return;
+                }
+            }
+            Console.WriteLine("É primo");
+        }
+    }
+}
