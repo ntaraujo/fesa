78eba4c596f97d804d3ab97e2d0fe82bf2301e7a
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-10-04 20:03:50 -0300

    aula20 classes

---
+++ 2sem/alg/aula20/aula20/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    enum Genero
+    {
+        Romance = 0
+    }
+
+    class Filme
+    {
+        private string titulo;
+        private string duracao;
+        private int faixaEtaria;
+        private Genero genero;
+        private string diretor;
+
+        public string getTitulo()
+        {
+            return this.titulo;
+        }
+        public void setTitulo(string titulo)
+        {
+            this.titulo = titulo;
+        }
+    }
+}
