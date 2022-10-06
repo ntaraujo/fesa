faf9a892b6c2b4e315c7d3a175963b16e385c6c3
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-10-06 19:45:23 -0300

    ator aula20

--- 2sem/alg/aula20/aula20/Class1.cs
+++ 2sem/alg/aula20/aula20/Class1.cs
@@ -10,6 +10,20 @@ namespace Model
     {
         Romance = 0
     }
+    
+    class Ator
+    {
+        private string nome;
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+    }
 
     class Filme
     {
@@ -18,6 +32,7 @@ namespace Model
         private int faixaEtaria;
         private Genero genero;
         private string diretor;
+        private List<Ator> atores = new List<Ator>();
 
         public string getTitulo()
         {
@@ -26,6 +41,21 @@ namespace Model
         public void setTitulo(string titulo)
         {
             this.titulo = titulo;
+        }
+
+        public void setAtores(List<Ator> atores)
+        {
+            this.atores = atores;
+        }
+
+        public void addAtor(Ator ator)
+        {
+            this.atores.Add(ator);
+        }
+
+        public List<Ator> getAtores()
+        {
+            return this.atores;
         }
     }
 }
--- 2sem/alg/aula20/aula20/Program.cs
+++ 2sem/alg/aula20/aula20/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
 namespace aula20
 {
@@ -10,6 +11,28 @@ namespace aula20
     {
         static void Main(string[] args)
         {
+            Filme filme1 = new Filme();
+            Filme filme2 = new Filme();
+
+            Ator ator1 = new Ator();
+            ator1.setNome("ator1");
+            Ator ator2 = new Ator();
+            ator2.setNome("ator2");
+
+            filme1.addAtor(ator1);
+            filme2.addAtor(ator2);
+
+            List<Ator> lista1 = filme1.getAtores();
+            List<Ator> lista2 = filme2.getAtores();
+
+            foreach (Ator a in lista1)
+            {
+                Console.WriteLine(a.getNome());
+            }
+            foreach (Ator a in lista2)
+            {
+                Console.WriteLine(a.getNome());
+            }
         }
     }
 }
