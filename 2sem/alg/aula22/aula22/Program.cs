f6403ec5a94ccd2f159ab8835444f6b7ce141cdf
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-10-17 20:38:00 -0300

    aula22

---
+++ 2sem/alg/aula22/aula22/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace aula22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EC = Curso();
+            Console.WriteLine("=================================");
+            Console.WriteLine("Curso: " + EC);
+        }
+    }
+}
