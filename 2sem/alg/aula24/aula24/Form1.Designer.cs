db905de9005b6e13657276d662f467087f1c4694
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-10-31 19:41:19 -0300

    aula 24

---
+++ 2sem/alg/aula24/aula24/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace aula24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.componentesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBoxListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exemplo1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exemplo1ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.componentesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // componentesToolStripMenuItem
+            // 
+            this.componentesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkBoxToolStripMenuItem,
+            this.checkBoxListToolStripMenuItem,
+            this.comboBoxToolStripMenuItem});
+            this.componentesToolStripMenuItem.Name = "componentesToolStripMenuItem";
+            this.componentesToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.componentesToolStripMenuItem.Text = "Componentes";
+            // 
+            // checkBoxToolStripMenuItem
+            // 
+            this.checkBoxToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exemplo1ToolStripMenuItem,
+            this.exemplo1ToolStripMenuItem1});
+            this.checkBoxToolStripMenuItem.Name = "checkBoxToolStripMenuItem";
+            this.checkBoxToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.checkBoxToolStripMenuItem.Text = "CheckBox";
+            // 
+            // checkBoxListToolStripMenuItem
+            // 
+            this.checkBoxListToolStripMenuItem.Name = "checkBoxListToolStripMenuItem";
+            this.checkBoxListToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.checkBoxListToolStripMenuItem.Text = "CheckBoxList";
+            // 
+            // comboBoxToolStripMenuItem
+            // 
+            this.comboBoxToolStripMenuItem.Name = "comboBoxToolStripMenuItem";
+            this.comboBoxToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.comboBoxToolStripMenuItem.Text = "ComboBox";
+            // 
+            // exemplo1ToolStripMenuItem
+            // 
+            this.exemplo1ToolStripMenuItem.Name = "exemplo1ToolStripMenuItem";
+            this.exemplo1ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exemplo1ToolStripMenuItem.Text = "exemplo 1";
+            this.exemplo1ToolStripMenuItem.Click += new System.EventHandler(this.exemplo1ToolStripMenuItem_Click);
+            // 
+            // exemplo1ToolStripMenuItem1
+            // 
+            this.exemplo1ToolStripMenuItem1.Name = "exemplo1ToolStripMenuItem1";
+            this.exemplo1ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.exemplo1ToolStripMenuItem1.Text = "exemplo 1";
+            this.exemplo1ToolStripMenuItem1.Click += new System.EventHandler(this.exemplo1ToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem componentesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkBoxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exemplo1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exemplo1ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem checkBoxListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem comboBoxToolStripMenuItem;
+    }
+}
+
---
+++ 2sem/alg/aula24/aula24/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exemplo1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("1");
+        }
+
+        private void exemplo1ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("2");
+        }
+    }
+}
