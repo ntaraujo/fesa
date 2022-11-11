984fa6f6538041f1322c187688e2df993627fe5e
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-11-10 22:39:45 -0300

    random colors and system sequence

--- 2sem/alg/Joguinho/Joguinho/FrmJogo.Designer.cs
+++ 2sem/alg/Joguinho/Joguinho/FrmJogo.Designer.cs
@@ -29,13 +29,68 @@ namespace Joguinho
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSalmon;
+            this.button1.Location = new System.Drawing.Point(231, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 150);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightGreen;
+            this.button2.Location = new System.Drawing.Point(387, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 150);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightBlue;
+            this.button3.Location = new System.Drawing.Point(231, 218);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 150);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightYellow;
+            this.button4.Location = new System.Drawing.Point(387, 218);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 150);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // FrmJogo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
             this.Name = "FrmJogo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmJogo";
@@ -45,5 +100,11 @@ namespace Joguinho
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        public System.Windows.Forms.Timer timer1;
     }
 }
--- 2sem/alg/Joguinho/Joguinho/FrmJogo.cs
+++ 2sem/alg/Joguinho/Joguinho/FrmJogo.cs
@@ -12,6 +12,16 @@ namespace Joguinho
 {
     public partial class FrmJogo : Form
     {
+        private int timeWaitInit = 0;
+        private Button[] buttons = new Button[4];
+        private Color[] colors = new Color[4];
+        private Color[] defaultColors = new Color[4];
+        private Random random = new Random();
+        private List<int> systemSequence = new List<int>();
+        private List<int> playerSequence = new List<int>();
+        private int round = 0;
+
+
         public FrmJogo()
         {
             InitializeComponent();
@@ -22,13 +32,70 @@ namespace Joguinho
 
         private void FrmJogo_Load(object sender, EventArgs e)
         {
+            buttons[0] = button1;
+            buttons[1] = button2;
+            buttons[2] = button3;
+            buttons[3] = button4;
 
+            colors[0] = Color.Red;
+            colors[1] = Color.Green;
+            colors[2] = Color.Blue;
+            colors[3] = Color.Yellow;
+
+            defaultColors[0] = Color.LightSalmon;
+            defaultColors[1] = Color.LightGreen;
+            defaultColors[2] = Color.LightBlue;
+            defaultColors[3] = Color.LightYellow;
         }
 
         private void FrmJogo_Closed(object sender, FormClosedEventArgs e)
         {
             FrmMenu frmMenu = new FrmMenu();
             frmMenu.Show();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (round == 0)
+            {
+                systemRound();
+            }
+        }
+
+        private void systemRound()
+        {
+            if (this.timeWaitInit < 500)
+            {
+                this.timeWaitInit += 100;
+                defaultColorize();
+            }
+            else
+            {
+                this.timeWaitInit = 0;
+                randomColorize();
+            }
+        }
+
+        private void randomColorize()
+        {
+            int index = random.Next(4);
+
+            buttons[index].BackColor = colors[index];
+
+            systemSequence.Add(index);
+        }
+
+        private void defaultColorize()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                buttons[i].BackColor = defaultColors[i];
+            }
+
+            if (systemSequence.Count == 4)
+            {
+                round = 1;
+            }
         }
     }
 }
