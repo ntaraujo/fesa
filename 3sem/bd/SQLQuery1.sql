0e86e2c41ae2e015678713f35606e24e86528141
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2023-02-03 22:23:16 -0300

    aula 11 bd

---
+++ 3sem/bd/SQLQuery1.sql
@@ -0,0 +1,33 @@
+USE pedidos
+
+SELECT * FROM EX2_PEDIDO
+
+INSERT INTO EX2_PEDIDO VALUES (7, null, '2023/03/02', 00007, 152.11)
+
+-- 1
+SELECT
+	p.codpedido,
+	c.nome
+FROM
+		EX2_PEDIDO p
+	INNER JOIN
+		EX2_CLIENTE c
+	ON (p.codcliente = c.codcliente)
+
+-- 2
+SELECT
+	pc.*,
+	count(i.numeroitem) AS qtd
+FROM
+		(
+			SELECT
+				p.codpedido,
+				c.nome
+			FROM EX2_PEDIDO p INNER JOIN EX2_CLIENTE c
+				ON (p.codcliente = c.codcliente)
+		) pc
+	INNER JOIN
+		EX2_ITEMPEDIDO i
+    ON (pc.codpedido = i.codpedido)
+GROUP BY
+	pc.codpedido, pc.nome
