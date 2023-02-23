53c166a06335589328c7898fef8359637da17d64
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2023-02-23 19:23:53 -0300

    poo ex01

---
+++ 3sem/poo/ex01/ex01/Jogo.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex01
+{
+    internal class Jogo
+    {
+        private int _codigo;
+        private string _nome;
+        private string _categoria;
+        private DateTime _data_de_lancamento;
+
+        public int codigo{
+            get {
+                return _codigo;
+            }
+            set {
+                if (value <= 0)
+                {
+                    throw new Exception("deve ser > 0");
+                }
+                else
+                {
+                    _codigo = value;
+                }
+            }
+        }
+        public string nome{
+            get {
+                return _nome;
+            }
+            set {
+                if (value.Trim().Length == 0)
+                {
+                    throw new Exception("obrigatório");
+                }
+                else
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        private List<string> _categorias_permitidas = new List<string>() {"ação", "luta", "tiro", "Esportes" };
+        public string categoria{
+            get {
+                return _categoria;
+            }
+            set {
+                if (!_categorias_permitidas.Contains(value))
+                {
+                    throw new Exception("válido apenas “ação”  “luta” “tiro” e “Esportes”");
+                }
+                else
+                {
+                    _categoria = value;
+                }
+            }
+        }
+        public DateTime data_de_lancamento { 
+            get {
+                return _data_de_lancamento;
+            }
+            set
+            {
+                if (value >= DateTime.Now)
+                {
+                    throw new Exception("deve ser  inferior à data atual");
+                }
+                else
+                {
+                    _data_de_lancamento = value;
+                }
+            }
+        }
+    }
+}
---
+++ 3sem/poo/ex01/ex01/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Jogo> lista_jogos = new List<Jogo>();
+            do
+            {
+                Jogo jogo_instancia = new Jogo();
+
+                lista_jogos.Add(jogo_instancia);
+
+                Console.WriteLine($"Cadastro do jogo ({lista_jogos.Count}/10):\n\n");
+
+                while (true)
+                {
+                    Console.Write("Código: ");
+                    try
+                    {
+                        jogo_instancia.codigo = int.Parse(Console.ReadLine());
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+
+                while (true)
+                {
+                    Console.Write("Nome: ");
+                    try
+                    {
+                        jogo_instancia.nome = Console.ReadLine();
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+
+                while (true)
+                {
+                    Console.Write("Categoria: ");
+                    try
+                    {
+                        jogo_instancia.categoria = Console.ReadLine();
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+
+                while (true)
+                {
+                    Console.Write("Data de Lançamento: ");
+                    try
+                    {
+                        jogo_instancia.data_de_lancamento = DateTime.Parse(Console.ReadLine());
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+
+                Console.Write("Adicionar mais um cadastro? s/N?");
+                if (Console.ReadLine().Trim().ToLower() != "s")
+                {
+                    break;
+                }
+
+            } while (lista_jogos.Count < 10);
+
+            Console.WriteLine("Todos os Cadastros:");
+
+            foreach (Jogo jogo_instancia in lista_jogos)
+            {
+                Console.WriteLine($"Código =  {jogo_instancia.codigo}; Nome = {jogo_instancia.nome} ; Categoria = {jogo_instancia.categoria} ; Data de Lançamento = {jogo_instancia.data_de_lancamento}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
